d0e21663ab99ab90afa16fa670c6e35c56677fae
Andoly <andoly.cs@hotmail.com>
2020-02-21 13:39:06 -0300

    Product while True

--- PyBasic/Basic_011.py
+++ PyBasic/Basic_011.py
@@ -1,7 +1,32 @@
+withdraw = float(input('Valor a ser sacado : '))
+total = withdraw
+ced = 100
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de R$ {ced}')
+        if ced == 100:
+            ced = 50
+        elif ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 5
+        elif ced == 5:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+
 total_spend = product1000 = menor = cont = 0
 product_cheap = ' '
+
 while True:
-    list_product = []
     name_product = str(input('Nome do produto : ')).strip()
     try:
         price = float(input(f'Preço {name_product} : '))
@@ -24,9 +49,9 @@ while True:
         register = str(input('Deseja adquirir um novo produto? [S / N] ')).strip().upper()[0]
     if register == 'N':
         break
+print(f'Saque de {withdraw} realizado com sucesso')
 print(f'\nO total da compra foi de R${total_spend}')
+print(f'Restou R$ {withdraw - total_spend}')
 print(f'{product1000} custaram mais de R$ 1000')
 print(f'O produto mais barato foi {product_cheap} custando R$ {menor}')
-
-
 
