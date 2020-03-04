f7daabc12bdb3e08ac34721a784e4a9dffa2e5d7
Andoly <andoly.cs@hotmail.com>
2020-03-04 11:16:29 -0300

    list within a list and dictionary within a list

--- PyBasic/Basic_024.py
+++ PyBasic/Basic_024.py
@@ -16,9 +16,26 @@ while True:
     answer = input('Continuar ? [S/N]').strip().upper()[0]
     if answer in 'N':
         break
-print(f'{"Atleta":^10}  {"Gols":^10}     {"Total de gols":^10}')
-for p in Atleta_list:
-    print(f'{p["Nome"]:^10}  {p["Gols"]}          {p["Total de gols"]}')
 
+print('ID', end=' ')
+for i in Atleta.keys():
+    print(f'{i:<20}', end=' ')
+print()
 
-print(f'\n{Atleta_list}')
+for k,v in enumerate(Atleta_list):
+    print(f'{(k)} ', end=' ')
+    for d in v.values():
+        print(f'{str(d):<20}', end=' ')
+    print()
+print('-'* 40)
+while True:
+    search = int(input('Deseja visualizar dados de qual jogador? [999 - SAIR] : '))
+    if search == 999:
+        break
+    if search >= len(Atleta_list):
+        print(f'Não existe atleta com o código {search} digitado.')
+    else:
+        print(f'Status do atleta : {Atleta_list[search]["Nome"]}')
+        for i, g in enumerate(Atleta_list[search]['Gols']):
+            print(f'Jogo {i+1} {Atleta_list[search]["Nome"]} fez {g} gols')
+        print()
